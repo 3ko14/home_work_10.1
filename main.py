25d3fc21b64214407eb487813826fe5ad0f29984
Илья Лесняк <3ko.lk19962812@gmail.com>
2024-06-28 13:13:04 +0300

    the first version

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.widget import mask_account_card, get_data
+
+print(mask_account_card("Visa Classic 6831982476737658"))
+
+
+print(mask_account_card("Счет 64686473678894779589"))
+
+
+print(get_data("2018-07-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from src.masks import get_mask_account, get_mask_kard
+
+
+def mask_account_card(number: str) -> str:
+    """Функция принимает строку и маскирует номер карты/счета"""
+    if len(number.split()[-1]) == 16:
+        new_number = get_mask_kard(number.split()[-1])
+        result = f"{number[:-16]}{new_number}"
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account(number.split()[-1])
+        result = f"{number[:-20]}{new_number}"
+    return result
+
+
+def get_data(old_data: str) -> str:
+    """Функция принимает строку и возвращает число месяц год"""
+    data_slice = old_data[0:10].split("-")
+    return ".".join(data_slice[::-1])
